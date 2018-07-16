79373847796fee2c2850d52394630b5b373d8de5
av.sorokin <A.Sorokin@mils-s.ru>
2018-07-16 18:20:10 +0500

    Перевод главы 3.3.3

--- JavaConcurency/src/main/java/net/jcip/examples/ch_3/ConnectionDispenser.java
+++ JavaConcurency/src/main/java/net/jcip/examples/ch_3/ConnectionDispenser.java
@@ -14,17 +14,14 @@ import java.sql.SQLException;
 public class ConnectionDispenser {
 	static String DB_URL = "jdbc:mysql://localhost/mydatabase";
 
-	private ThreadLocal<Connection> connectionHolder
-			= new ThreadLocal<Connection>() {
+	private ThreadLocal<Connection> connectionHolder = new ThreadLocal<Connection>() {
 		public Connection initialValue() {
 			try {
 				return DriverManager.getConnection(DB_URL);
 			} catch (SQLException e) {
 				throw new RuntimeException("Unable to acquire Connection, e");
 			}
-		}
-
-		;
+		};
 	};
 
 	public Connection getConnection() {
