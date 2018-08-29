5ff627fb21165d78fa02db811ffb8e5c71cf7bb3
av.sorokin <A.Sorokin@mils-s.ru>
2018-08-29 13:19:03 +0500

    Перевод главы 5.6

--- JavaConcurency/src/main/java/net/jcip/examples/ch_5/Memoizer2.java
+++ JavaConcurency/src/main/java/net/jcip/examples/ch_5/Memoizer2.java
@@ -12,16 +12,16 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class Memoizer2<A, V> implements Computable<A, V> {
 	private final Map<A, V> cache = new ConcurrentHashMap<A, V>();
-	private final Computable<A, V> c;
+	private final Computable<A, V> comp;
 
-	public Memoizer2(Computable<A, V> c) {
-		this.c = c;
+	public Memoizer2(Computable<A, V> comp) {
+		this.comp = comp;
 	}
 
 	public V compute(A arg) throws InterruptedException {
 		V result = cache.get(arg);
 		if (result == null) {
-			result = c.compute(arg);
+			result = comp.compute(arg);
 			cache.put(arg, result);
 		}
 		return result;
